3715eed0289400dae9e51223d75212b77be833aa
tomaslebre <20220907@iade.pt>
2025-05-24 12:40:42 +0100

    mosquitto done

--- web/src/pages/controller/index.jsx
+++ web/src/pages/controller/index.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom"; // Importa o Link do React Router
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const enviarComando = (comando) => {
+  fetch("http://localhost:1880/enviar-comando", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ comando })
+  })
+  .then(response => response.json())
+  .then(data => console.log('Resposta Node-RED:', data))
+  .catch(error => console.error('Erro no fetch:', error));
+};
+
+
+
 const Controller = () => {
   return (
     <div className="d-flex flex-column align-items-center gap-2 mt-4">
-      <button className="btn btn-primary">▲ Frente</button>
+      <button className="btn btn-primary" onClick={() => enviarComando("frente")}>▲ Frente</button>
       <div className="d-flex gap-2">
-        <button className="btn btn-secondary">◀ Esquerda</button>
-        <button className="btn btn-secondary">▶ Direita</button>
+        <button className="btn btn-secondary" onClick={() => enviarComando("esquerda")}>◀ Esquerda</button>
+        <button className="btn btn-secondary" onClick={() => enviarComando("direita")}>▶ Direita</button>
       </div>
-      <button className="btn btn-danger">▼ Trás</button>
+      <button className="btn btn-danger" onClick={() => enviarComando("tras")}>▼ Trás</button>
 
       {/* Botão de Voltar para a Página Principal */}
       <Link to="/" className="btn btn-warning mt-3">
